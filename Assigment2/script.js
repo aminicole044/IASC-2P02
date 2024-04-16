fbf250102f2675a4c2859c49bd807cbb04dafaa5
aminicole044 <al22wd@brocku.ca>
2024-04-16 12:06:51 -0400

    final

--- Assigment2/script.js
+++ Assigment2/script.js
@@ -1,217 +1,238 @@
-import * as THREE from "three"
-import * as dat from "lil-gui"
-import { OrbitControls } from "OrbitControls"
-
+import * as THREE from "three" 
+import * as dat from "lil-gui" 
+import { OrbitControls } from "OrbitControls" 
 /**********
 ** SETUP **
-***********/
-// Sizes
-const sizes = {
-    width: window.innerWidth / 2.5,
-    height: window.innerHeight / 1.5,
-    aspectRatio: 1
-}
+***********/ 
 
-/***********
-** SCENE **
-***********/
-// Canvas
-const canvas = document.querySelector('.webgl')
+// Sizes 
+const sizes = { 
+    width: window.innerWidth / 2.5, 
+    height: window.innerHeight / 1.5, 
+    aspectRatio: 1 
+} 
 
-// Scene
-const scene = new THREE.Scene()
-scene.background = new THREE.Color('grey')
+/*********** 
+** SCENE ** 
+***********/ 
+// Canvas 
+const canvas = document.querySelector('.webgl') 
 
-// Camera
-const camera = new THREE.PerspectiveCamera(
-    75,
-    sizes.aspectRatio,
-    0.1,
-    100
-)
-camera.position.set(0, 0, 20)
-scene.add(camera)
+// Scene 
 
-// Renderer
-const renderer = new THREE.WebGLRenderer({
-    canvas: canvas,
-    antialias: true
-})
-renderer.setSize(sizes.width, sizes.height)
+const scene = new THREE.Scene() 
+scene.background = new THREE.Color('lightgreen') 
 
-// Orbit Controls
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
+// Camera 
+const camera = new THREE.PerspectiveCamera( 
+    75, 
+    sizes.aspectRatio, 
+    0.1, 
+    100 
+) 
 
-/***********
-** LIGHTS **
-************/
-// Directional Light
-const directionalLight = new THREE.DirectionalLight(0x404040, 100)
-scene.add(directionalLight)
+camera.position.set(0, 0, 20) 
+scene.add(camera) 
 
-/***********
-** MESHES **
-************/
-// Cube Geometry
-const cubeGeometry = new THREE.BoxGeometry(0.5, 0.5, 0.5)
+// Renderer 
+const renderer = new THREE.WebGLRenderer({ 
+    canvas: canvas, 
+    antialias: true 
+}) 
 
-// Cube Materials
-const redMaterial = new THREE.MeshStandardMaterial({
-    color: new THREE.Color('red')
-})
-const greenMaterial = new THREE.MeshStandardMaterial({
-    color: new THREE.Color('black')
-})
-const blueMaterial = new THREE.MeshStandardMaterial({
-    color: new THREE.Color('pink')
-})
+renderer.setSize(sizes.width, sizes.height) 
 
-const drawCube = (i, material) =>
-{
-    const cube = new THREE.Mesh(cubeGeometry, material)
-    cube.position.x = (Math.random() - 0.5) * 10
-    cube.position.z = (Math.random() - 0.5) * 10
-    cube.position.y = i - 10
+// Orbit Controls 
+const controls = new OrbitControls(camera, canvas) 
+controls.enableDamping = true 
 
-    cube.rotation.x = Math.random() * 2 * Math.PI
-    cube.rotation.y = Math.random() * 2 * Math.PI
-    cube.rotation.z = Math.random() * 2 * Math.PI
+/*********** 
+** LIGHTS ** 
+************/ 
 
-    cube.randomizer = Math.random()
+// Directional Light 
 
-    scene.add(cube)
-}
+const directionalLight = new THREE.DirectionalLight(0x404040, 100) 
+scene.add(directionalLight) 
 
+/*********** 
+** MESHES ** 
+************/ 
 
-/**********************
-** TEXT PARSERS & UI **
-***********************/
-let preset = {}
+// Cube Geometry 
+const cubeGeometry = new THREE.DodecahedronGeometry() 
 
-const uiobj = {
-    text: '',
-    textArray: [],
-    term1: 'cupboard',
-    term2: 'hat',
-    term3: 'broom',
-    rotateCamera: false,
-   
-}
+// Cube Materials 
+const redMaterial = new THREE.MeshStandardMaterial({ 
+    color: new THREE.Color('red') 
+}) 
 
+const greenMaterial = new THREE.MeshStandardMaterial({ 
+    color: new THREE.Color('green') 
+}) 
 
-// Parse Text and Terms
-const parseTextandTerms = () =>
-{   
-    // Strip periods and downcase text
-    const parsedText = uiobj.text.replaceAll(".", "").toLowerCase()
-    //console.log(parsedText)
+const blueMaterial = new THREE.MeshStandardMaterial({ 
+    color: new THREE.Color('pink') 
+}) 
 
-    // Tokenize text
-    uiobj.textArray = parsedText.split(/[^\w']+/)
-    //console.log(uiobj.textArray)
+ 
+const drawCube = (i, material) => 
+{ 
+    const cube = new THREE.Mesh(cubeGeometry, material) 
+    cube.position.x = (Math.random() - 0.5) * 10 
+    cube.position.z = (Math.random() - 0.5) * 10 
+    cube.position.y = i - 10 
 
-    // Find term 1
-    findTermInParsedText(uiobj.term1, redMaterial)
+    cube.rotation.x = Math.random() * 2 * Math.PI 
+    cube.rotation.y = Math.random() * 2 * Math.PI 
+    cube.rotation.z = Math.random() * 2 * Math.PI 
 
-    // Find term 2
-    findTermInParsedText(uiobj.term2, greenMaterial)
+    cube.randomizer = Math.random() 
+    scene.add(cube) 
+} 
+/********************** 
+** TEXT PARSERS & UI ** 
+***********************/ 
+let preset = {} 
 
-    // Find term 3
-    findTermInParsedText(uiobj.term3, blueMaterial)
+const uiobj = { 
+    text: '', 
+    textArray: [], 
+    term1: 'rose', 
+    term2: 'thorns', 
+    term3: 'love',
+    rotateCamera: false, 
+    animateBubbles: false  
+} 
 
-}
+// Parse Text and Terms 
+const parseTextandTerms = () => 
+{    
+    // Strip periods and downcase text 
+    const parsedText = uiobj.text.replaceAll(".", "").toLowerCase() 
+    //console.log(parsedText) 
 
+    // Tokenize text 
+    uiobj.textArray = parsedText.split(/[^\w']+/) 
+    //console.log(uiobj.textArray) 
 
-const findTermInParsedText = (term, material) =>
-{
-    for (let i=0; i < uiobj.textArray.length; i++)
-    {
-        //console.log(i, uiobj.textArray[i])
-        if(uiobj.textArray[i] === term)
-        {
-         //console.log(i, term)
-         // convert i into n, which is a value between 0 and 20
-         const n = (100 / uiobj.textArray.length) * i * 0.2
-         
-         // call drawCube function 5 times using converted n value
-         for(let a=0; a < 5; a++)
-         {
-            drawCube(n, material)
-         }
+    // Find term 1 
+    findTermInParsedText(uiobj.term1, redMaterial) 
 
-        }
-    }
-}
+    // Find term 2 
+    findTermInParsedText(uiobj.term2, greenMaterial) 
 
+    // Find term 3 
+    findTermInParsedText(uiobj.term3, blueMaterial) 
+} 
 
-// Text Parsers
-// Load source text
-fetch("https://raw.githubusercontent.com/amephraim/nlp/master/texts/J.%20K.%20Rowling%20-%20Harry%20Potter%204%20-%20The%20Goblet%20of%20Fire.txt")
-    .then(response => response.text())
-    .then((data) =>
-    {
-        uiobj.text = data
-        parseTextandTerms()
-    }
-    )
-// UI/
-const ui = new dat.GUI({
-    container: document.querySelector('#parent1')
-})                                                                                                                     
+ 
+const findTermInParsedText = (term, material) => 
+{ 
 
-// Interaction Folders
+    for (let i=0; i < uiobj.textArray.length; i++) 
+    { 
+        //console.log(i, uiobj.textArray[i]) 
+        if(uiobj.textArray[i] === term) 
+        { 
 
-    // Cubes Folder
-    const cubesFolder = ui.addFolder('Filter Terms')
+         //console.log(i, term) 
+         // convert i into n, which is a value between 0 and 20 
 
-    cubesFolder
-        .add(redMaterial, 'visible')
-        .name(`${uiobj.term1}`)
+         const n = (100 / uiobj.textArray.length) * i * 0.2 
+         // call drawCube function 5 times using converted n value 
 
-    cubesFolder
-        .add(greenMaterial, 'visible')
-        .name(`${uiobj.term2}`)
+         for(let a=0; a < 5; a++) 
 
-    cubesFolder
-        .add(blueMaterial, 'visible')
-        .name(`${uiobj.term3}`)
+         { 
+            drawCube(n, material) 
+         } 
+        } 
+    } 
+} 
 
-    // Camera Folder
-    const cameraFolder = ui.addFolder('Camera')
+// Text Parsers 
+// Load source text 
+fetch("https://raw.githubusercontent.com/monicedy/monicedy.github.io/main/_posts/Prince.txt") 
+    .then(response => response.text()) 
+    .then((data) => 
+    { 
+        uiobj.text = data 
+        parseTextandTerms() 
+    } 
+    ) 
 
-    cameraFolder
-        .add(uiobj, 'rotateCamera')
+// UI/ 
+const ui = new dat.GUI({ 
+    container: document.querySelector('#parent1') 
+})                                                                                                                      
+
+// Interaction Folders 
+
+    // Cubes Folder 
+    const cubesFolder = ui.addFolder('Filter Terms') 
+    cubesFolder 
+        .add(redMaterial, 'visible') 
+        .name(`${uiobj.term1}`) 
+
+    cubesFolder 
+        .add(greenMaterial, 'visible') 
+        .name(`${uiobj.term2}`) 
+
+    cubesFolder 
+        .add(blueMaterial, 'visible') 
+        .name(`${uiobj.term3}`) 
+
+        cubesFolder 
+        .add(uiobj, 'animateBubbles') 
+        .name('Animate Bubbles') 
+
+    // Camera Folder 
+    const cameraFolder = ui.addFolder('Camera') 
+
+    cameraFolder 
+        .add(uiobj, 'rotateCamera') 
         .name('Rotate Camera')
 
-/*******************
-** ANIMATION LOOP **
-********************/
-const clock = new THREE.Clock()
+/******************* 
+** ANIMATION LOOP ** 
+********************/ 
 
-// Animate
-const animation = () =>
-{
-    // Return elapsedTime
-    
-    const elapsedTime = clock.getElapsedTime()
+const clock = new THREE.Clock() 
+// Animate 
 
-    // Orbit Controls
-    controls.update()
+const animation = () => 
+{ 
+    // Return elapsedTime 
+    const elapsedTime = clock.getElapsedTime() 
 
-    // Camera Rotation
-    if(uiobj.rotateCamera)
-    {
-        camera.position.x = Math.sin(elapsedTime * 0.2) * 16
-        camera.position.z = Math.cos(elapsedTime * 0.2) * 16
-    }
+    // Orbit Controls 
+    controls.update() 
+
+    // Camera Rotation 
+    if(uiobj.rotateCamera) 
+    { 
+        camera.position.x = Math.sin(elapsedTime * 0.2) * 16 
+        camera.position.z = Math.cos(elapsedTime * 0.2) * 16 
+    } 
+    //animate bubbles 
+if(uiobj.animateBubbles){ 
+    for(let i=0; i <scene.children.length; i++) 
+    { 
+     if(scene.children[i].type === "Mesh") 
+     { 
+         scene.children[i].scale.x = Math.sin(elapsedTime) 
+         scene.children[i].scale.y = Math.sin(elapsedTime) 
+         scene.children[i].scale.z = Math.sin(elapsedTime) 
+     } 
+    } 
+ }
 
 
-    // Renderer
-    renderer.render(scene, camera)
+    // Renderer 
+    renderer.render(scene, camera) 
+    // Request next frame 
+    window.requestAnimationFrame(animation) 
 
-    // Request next frame
-    window.requestAnimationFrame(animation)
-}
-
-animation()
+} 
+animation() 
--- Assigment2/script2.js
+++ Assigment2/script2.js
@@ -1,232 +1,240 @@
-import * as THREE from "three"
-import * as dat from "lil-gui"
-import { OrbitControls } from "OrbitControls"
+import * as THREE from "three" 
+import * as dat from "lil-gui" 
+import { OrbitControls } from "OrbitControls" 
+/********** 
+** SETUP ** 
+***********/ 
+// Sizes 
+const sizes = { 
+    width: window.innerWidth / 2.5, 
+    height: window.innerHeight / 1.5, 
+    aspectRatio: 1 
+} 
+/*********** 
+** SCENE ** 
+***********/ 
+// Canvas 
+const canvas = document.querySelector('.webgl2') 
+// Scene 
+const scene = new THREE.Scene() 
+scene.background = new THREE.Color('lightblue') 
 
 
+// Camera 
+const camera = new THREE.PerspectiveCamera( 
+    75, 
+    sizes.aspectRatio, 
+    0.1, 
+    100 
+) 
+camera.position.set(0, 0, 20) 
+scene.add(camera) 
 
-/**********
-** SETUP **
-***********/
-// Sizes
-const sizes = {
-    width: window.innerWidth / 2.5,
-    height: window.innerHeight / 1.5,
-    aspectRatio: 1
-}
+// Renderer 
 
-/***********
-** SCENE **
-***********/
-// Canvas
-const canvas = document.querySelector('.webgl2')
+const renderer = new THREE.WebGLRenderer({ 
+    canvas: canvas, 
+    antialias: true 
+}) 
 
-// Scene
-const scene = new THREE.Scene()
-scene.background = new THREE.Color('grey')
+renderer.setSize(sizes.width, sizes.height) 
 
-// Camera
-const camera = new THREE.PerspectiveCamera(
-    75,
-    sizes.aspectRatio,
-    0.1,
-    100
-)
-camera.position.set(0, 0, 20)
-scene.add(camera)
+// Orbit Controls 
 
-// Renderer
-const renderer = new THREE.WebGLRenderer({
-    canvas: canvas,
-    antialias: true
-})
-renderer.setSize(sizes.width, sizes.height)
+const controls = new OrbitControls(camera, canvas) 
+controls.enableDamping = true 
 
-// Orbit Controls
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
+/*********** 
+** LIGHTS ** 
+************/ 
+// Directional Light 
 
-/***********
-** LIGHTS **
-************/
-// Directional Light
-const directionalLight = new THREE.DirectionalLight(0x404040, 100)
-scene.add(directionalLight)
+const directionalLight = new THREE.DirectionalLight(0x404040, 100) 
+scene.add(directionalLight) 
 
-/***********
-** MESHES **
-************/
-// Cube Geometry
-const sphereGeometry = new THREE.SphereGeometry(0.5)
+/*********** 
+** MESHES ** 
+************/ 
+// Cube Geometry 
 
-// Cube Materials
-const redMaterial = new THREE.MeshStandardMaterial({
-    color: new THREE.Color('green')
-})
-const greenMaterial = new THREE.MeshStandardMaterial({
-    color: new THREE.Color('aqua')
-})
-const blueMaterial = new THREE.MeshStandardMaterial({
-    color: new THREE.Color('blue')
-})
+const sphereGeometry = new THREE.SphereGeometry(0.5) 
+// Cube Materials 
 
-const drawSphere = (i, material) =>
+const redMaterial = new THREE.MeshStandardMaterial({ 
+    color: new THREE.Color('yellow') 
+}) 
+
+const greenMaterial = new THREE.MeshStandardMaterial({ 
+    color: new THREE.Color('green') 
+}) 
+
+const blueMaterial = new THREE.MeshStandardMaterial({ 
+    color: new THREE.Color('darkblue') 
+}) 
+
+const pinkMaterial = new THREE.MeshStandardMaterial({  
+    color: new THREE.Color('orange')  
+}) 
+
+const yellowMaterial = new THREE.MeshStandardMaterial({  
+    color: new THREE.Color('crimson')  
+})  
+
+const purpleMaterial = new THREE.MeshStandardMaterial({  
+    color: new THREE.Color('blue')  
+})  
+
+const drawSphere = (i, material) => 
 {
-    const sphere = new THREE.Mesh(sphereGeometry, material)
-    sphere.position.x = (Math.random() - 0.5) * 10
-    sphere.position.z = (Math.random() - 0.5) * 10
-    sphere.position.y = i - 10
+    const sphere = new THREE.Mesh(sphereGeometry, material) 
+    sphere.position.x = (Math.random() - 0.5) * 80 
+    sphere.position.z = (Math.random() - 0.5) * 80 
+    sphere.position.y = i - 10 
 
-    sphere.rotation.x = Math.random() * 2 * Math.PI
-    sphere.rotation.y = Math.random() * 2 * Math.PI
+    sphere.rotation.x = Math.random() * 2 * Math.PI 
+    sphere.rotation.y = Math.random() * 2 * Math.PI 
     sphere.rotation.z = Math.random() * 2 * Math.PI
 
-    scene.add(sphere)
-}
-
-
-/**********************
-** TEXT PARSERS & UI **
-***********************/
-let preset = {}
-
-const uiobj = {
-    text: '',
-    textArray: [],
-    term1: 'dudley',
-    term2: 'snape',
-    term3: 'malfoy',
-    rotateCamera: false,
-    animateBubbles: false
-   
-}
-
-
-// Parse Text and Terms
-const parseTextandTerms = () =>
-{
-    // Strip periods and downcase text
-    const parsedText = uiobj.text.replaceAll(".", "").toLowerCase()
-    //console.log(parsedText)
-
-    // Tokenize text
-    uiobj.textArray = parsedText.split(/[^\w']+/)
-    //console.log(uiobj.textArray)
-
-    // Find term 1
-    findTermInParsedText(uiobj.term1, redMaterial)
-
-    // Find term 2
-    findTermInParsedText(uiobj.term2, greenMaterial)
-
-    // Find term 3
-    findTermInParsedText(uiobj.term3, blueMaterial)
+    scene.add(sphere) 
 
 }
+/********************** 
+** TEXT PARSERS & UI ** 
+***********************/ 
+let preset = {} 
+const uiobj = { 
+    text: '', 
+    textArray: [], 
+    term1: 'prince', 
+    term2: 'narrator', 
+    term3: 'king', 
+    term4: 'fox',  
+    term5: 'lamplighter',  
+    term6: 'geographer',  
+    rotateCamera: false
+} 
+// Parse Text and Terms 
+const parseTextandTerms = () => 
+{ 
+    // Strip periods and downcase text 
+    const parsedText = uiobj.text.replaceAll(".", "").toLowerCase() 
 
-const findTermInParsedText = (term, material) =>
-{
-    for (let i=0; i < uiobj.textArray.length; i++)
-    {
-        //console.log(i, uiobj.textArray[i])
-        if(uiobj.textArray[i] === term)
-        {
-         //console.log(i, term)
-         // convert i into n, which is a value between 0 and 20
-         const n = (100 / uiobj.textArray.length) * i * 0.2
-         
-         // call drawCube function 5 times using converted n value
-         for(let a=0; a < 5; a++)
+    //console.log(parsedText) 
+    // Tokenize text 
+    uiobj.textArray = parsedText.split(/[^\w']+/) 
+
+    //console.log(uiobj.textArray) 
+    // Find term 1 
+    findTermInParsedText(uiobj.term1, redMaterial) 
+
+    // Find term 2 
+    findTermInParsedText(uiobj.term2, greenMaterial) 
+
+    // Find term 3 
+    findTermInParsedText(uiobj.term3, blueMaterial) 
+
+    findTermInParsedText(uiobj.term4, pinkMaterial)  
+    // Find term 2  
+    findTermInParsedText(uiobj.term5, yellowMaterial)  
+
+    findTermInParsedText(uiobj.term6, purpleMaterial)  
+} 
+
+ 
+const findTermInParsedText = (term, material) => 
+{ 
+    for (let i=0; i < uiobj.textArray.length; i++) 
+    { 
+        //console.log(i, uiobj.textArray[i]) 
+        if(uiobj.textArray[i] === term) 
+        { 
+         //console.log(i, term) 
+         // convert i into n, which is a value between 0 and 20 
+         const n = (100 / uiobj.textArray.length) * i * 0.2 
+
+         // call drawCube function 5 times using converted n value 
+         for(let a=0; a < 8; a++) 
          {
             drawSphere(n, material)
-         }
+         } 
+        } 
+    } 
+} 
+// Text Parsers 
 
-        }
-    }
-}
+// Load source text 
 
+fetch("https://raw.githubusercontent.com/monicedy/monicedy.github.io/main/_posts/Prince.txt") 
+    .then(response => response.text()) 
+    .then((data) => 
+    { 
+        uiobj.text = data 
+        parseTextandTerms() 
+    } 
+    ) 
 
-// Text Parsers
-// Load source text
-fetch("https://raw.githubusercontent.com/amephraim/nlp/master/texts/J.%20K.%20Rowling%20-%20Harry%20Potter%204%20-%20The%20Goblet%20of%20Fire.txt")
-    .then(response => response.text())
-    .then((data) =>
-    {
-        uiobj.text = data
-        parseTextandTerms()
-    }
-    )
-// UI/
-const ui = new dat.GUI({
-    container: document.querySelector('#parent2')
-})                                                                                                                     
+// UI/ 
 
-// Interaction Folders
+const ui = new dat.GUI({ 
+    container: document.querySelector('#parent2') 
+})                                                                                                                      
+// Interaction Folders 
+    // Cubes Folder 
+    const spheresFolder = ui.addFolder('Filter Terms') 
+    spheresFolder 
+        .add(redMaterial, 'visible') 
+        .name(`${uiobj.term1}`) 
 
-    // Cubes Folder
-    const spheresFolder = ui.addFolder('Filter Terms')
+        spheresFolder 
+        .add(greenMaterial, 'visible') 
+        .name(`${uiobj.term2}`) 
 
-    spheresFolder
-        .add(redMaterial, 'visible')
-        .name(`${uiobj.term1}`)
+        spheresFolder 
+        .add(blueMaterial, 'visible') 
+        .name(`${uiobj.term3}`) 
 
-        spheresFolder
-        .add(greenMaterial, 'visible')
-        .name(`${uiobj.term2}`)
+        spheresFolder 
+        .add(pinkMaterial, 'visible')  
+        .name(`${uiobj.term4}`)  
 
-        spheresFolder
-        .add(blueMaterial, 'visible')
-        .name(`${uiobj.term3}`)
+        spheresFolder  
+        .add(yellowMaterial, 'visible')  
+        .name(`${uiobj.term5}`)  
 
-        spheresFolder
-        .add(uiobj, 'animateBubbles')
-        .name('Animate Bubbles')
-        
-    // Camera Folder
-    const cameraFolder = ui.addFolder('Camera')
+        spheresFolder  
+        .add(purpleMaterial, 'visible')  
+        .name(`${uiobj.term6}`)  
 
-    cameraFolder
-        .add(uiobj, 'rotateCamera')
-        .name('Rotate Camera')
+    // Camera Folder 
+    const cameraFolder = ui.addFolder('Camera') 
+    cameraFolder 
+        .add(uiobj, 'rotateCamera') 
+        .name('Rotate Camera') 
 
-/*******************
-** ANIMATION LOOP **
-********************/
-const clock = new THREE.Clock()
+/******************* 
+** ANIMATION LOOP ** 
+********************/ 
+const clock = new THREE.Clock() 
 
-// Animate
-const animation = () =>
+// Animate 
+const animation = () => 
 {
-    // Return elapsedTime
-    const elapsedTime = clock.getElapsedTime()
+    // Return elapsedTime 
+    const elapsedTime = clock.getElapsedTime() 
+    // Orbit Controls 
+    controls.update() 
+    // Camera Rotation 
 
-    // Orbit Controls
-    controls.update()
+    if(uiobj.rotateCamera) 
 
-    // Camera Rotation
-    if(uiobj.rotateCamera)
-    {
-        camera.position.x = Math.sin(elapsedTime * 0.2) * 16
-        camera.position.z = Math.cos(elapsedTime * 0.2) * 16
-    }
-
-    //animate bubbles
-if(uiobj.animateBubbles){
-   for(let i=0; i <scene.children.length; i++)
-   {
-    if(scene.children[i].type === "Mesh")
-    {
-        scene.children[i].scale.x = Math.sin(elapsedTime)
-        scene.children[i].scale.y = Math.sin(elapsedTime)
-        scene.children[i].scale.z = Math.sin(elapsedTime)
-    }
-   }
-}
-
+    { 
+        camera.position.x = Math.sin(elapsedTime * 0.2) * 16 
+        camera.position.z = Math.cos(elapsedTime * 0.2) * 16 
+    } 
     // Renderer
-    renderer.render(scene, camera)
+    renderer.render(scene, camera) 
+    // Request next frame 
 
-    // Request next frame
-    window.requestAnimationFrame(animation)
-}
-
-animation()
+    window.requestAnimationFrame(animation) 
+} 
+animation() 
